611a52de70591f0c585a82854004a1df7441bea0
Bhishek-Parmar <itsbhishek@gmail.com>
2023-05-07 14:15:06 +0530

    initial commit

---
+++ src/components/AddNote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./addnote.css";
+const AddNote = () => {
+  return (
+    <div className="addNote">
+      <div className="createNote"></div>
+    </div>
+  );
+};
+
+export default AddNote;
---
+++ src/components/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./note.css";
+const Note = () => {
+  return (
+    <div className="card">
+      <h2 className="title">Title</h2>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, et voluptatum molestias modi, quas hic voluptas quae
+          atque corporis quisquam velit ex enim, voluptates odit officiis labore
+          ut sint reiciendis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Note;
